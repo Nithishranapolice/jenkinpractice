e0615f5a740a20c4159e67388b2858e5d09b3d7a
nithi <nithi@Nithish>
2026-01-03 13:10:29 +0530

    third commit

---
+++ src/main/java/javaprograms/addTwoNumbers.java
@@ -0,0 +1,14 @@
+package javaprograms;
+
+public class addTwoNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=25;
+		int c=a+b;
+		System.out.println("The addition of number is : "+c);
+
+	}
+
+}
